4e4f476f6557552e133d38fc72bac72d90eef479
Marek <marek.kicnav@gmail.com>
2016-03-19 14:47:57 +0100

    added control of arguments and result of crypto func

--- src/server/serverManager.cpp
+++ src/server/serverManager.cpp
@@ -12,6 +12,8 @@
 #include <sstream>
 
 #define NUMBER_OF_ITERATIONS 1000
+#define SALT_LENGTH 32
+#define PBKDF2_LENGTH 64
 
 int char_conversion = (sizeof(unsigned char) * 2);
 
@@ -73,14 +75,14 @@ void ServerManager::removeUserFromDb(std::string userName) {
 	//TODO Marek
 	if (!m_database.removeUser(userName))
 	{
-		std::cerr << "Client remove from database failed";
+		std::cerr << "Client remove from database failed\n";
 	}
 }
 
 void ServerManager::kickUser(std::string userName) {
 	//TODO Marek
 	//clientLogOut a clientDisconnect
-	std::string message = "You were kicked by server";
+	std::string message = "You were kicked by server\n";
 
 	for (auto it = m_clients.begin(); it < m_clients.end(); ++it)
 	{
@@ -103,47 +105,44 @@ bool ServerManager::userRegistration(std::string userName, std::string password)
 
 	if (password.length() < 8)
 	{
+		std::cerr << "Password was too short (length at least 8 characters is required)";
 		return false;
 	}
 
-	unsigned char salt[32];
-	unsigned char pbkdf2_output[64];
+	unsigned char salt[SALT_LENGTH];
+	unsigned char pbkdf2_output[PBKDF2_LENGTH];
 	const mbedtls_md_info_t* md_info = mbedtls_md_info_from_type(MBEDTLS_MD_SHA512);
 	mbedtls_md_context_t md_ctx;
 	mbedtls_entropy_context entropy;
 	mbedtls_ctr_drbg_context ctr_drbg;
 	char *personalization = "nahodne_slova_na_zvysenie_entropie_toto_nie_je_seed";
 	std::ostringstream conversion_stream;
+	int result = 0;
 
 	mbedtls_entropy_init(&entropy);
 	mbedtls_ctr_drbg_init(&ctr_drbg);
 	mbedtls_entropy_add_source(&entropy, mbedtls_platform_entropy_poll, nullptr, 512, MBEDTLS_ENTROPY_SOURCE_STRONG);
-	mbedtls_entropy_add_source(&entropy, mbedtls_hardclock_poll, nullptr, 512, MBEDTLS_ENTROPY_SOURCE_WEAK);
-	mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, 
+	mbedtls_entropy_add_source(&entropy, mbedtls_hardclock_poll, nullptr, 64, MBEDTLS_ENTROPY_SOURCE_WEAK);
+	result += mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, 
 		reinterpret_cast<const unsigned char *>(personalization), strlen(personalization));
-	mbedtls_ctr_drbg_random(&ctr_drbg, salt, 32);
+	result += mbedtls_ctr_drbg_random(&ctr_drbg, salt, SALT_LENGTH);
 
-	for (size_t i = 0; i < 32; ++i)
+	for (size_t i = 0; i < SALT_LENGTH; ++i)
 	{
 		conversion_stream << std::hex << std::setw(char_conversion) << std::setfill('0') << static_cast<int>(salt[i]);
 	}
 
-	/*dobra konverzia naspet
-	for (size_t i = 0; i < salt2.length(); i += char_conversion, ++j) {
-		std::string byte = salt2.substr(i, char_conversion);
-		salt_char[j] = static_cast<char>(static_cast<int>(strtol(byte.c_str(), nullptr, char_conversion * 8)));
-	}*/
-
 	std::string salt_string = conversion_stream.str();
+	
 	conversion_stream.clear();
 	conversion_stream.str("");
 
 	mbedtls_md_init(&md_ctx);
-	mbedtls_md_setup(&md_ctx, md_info, 1);
-	mbedtls_pkcs5_pbkdf2_hmac(&md_ctx, reinterpret_cast<const unsigned char*>(password.c_str()),
-		password.length(), salt, 32, NUMBER_OF_ITERATIONS, 64, pbkdf2_output);
+	result += mbedtls_md_setup(&md_ctx, md_info, 1);
+	result += mbedtls_pkcs5_pbkdf2_hmac(&md_ctx, reinterpret_cast<const unsigned char*>(password.c_str()),
+		password.length(), salt, SALT_LENGTH, NUMBER_OF_ITERATIONS, PBKDF2_LENGTH, pbkdf2_output);
 		
-	for (size_t i = 0; i < 64; ++i)
+	for (size_t i = 0; i < PBKDF2_LENGTH; ++i)
 	{
 		conversion_stream << std::hex << std::setw(char_conversion) << std::setfill('0') << static_cast<int>(pbkdf2_output[i]);
 	}
@@ -154,7 +153,13 @@ bool ServerManager::userRegistration(std::string userName, std::string password)
 	mbedtls_ctr_drbg_free(&ctr_drbg);
 	mbedtls_entropy_free(&entropy);
 
-	return m_database.insertUser(UserDatabaseRow(userName, password_string, salt_string));
+	if(result == 0)
+	{
+		return m_database.insertUser(UserDatabaseRow(userName, password_string, salt_string));
+	}
+
+	std::cerr << "Some crypto fuction failed\n";
+	return false;
 }
 
 bool ServerManager::userAuthentication(std::string userName, std::string password) {
@@ -164,13 +169,14 @@ bool ServerManager::userAuthentication(std::string userName, std::string passwor
 	UserDatabaseRow row = m_database.getUser(userName);
 	std::string salt = row.getSalt();
 	std::string row_hash = row.getPassword();
-	unsigned char salt_char[32];
-	unsigned char row_hash_char[64];
-	unsigned char pbkdf2_output[64];
+	unsigned char salt_char[SALT_LENGTH];
+	unsigned char row_hash_char[PBKDF2_LENGTH];
+	unsigned char pbkdf2_output[PBKDF2_LENGTH];
 	const mbedtls_md_info_t* md_info = mbedtls_md_info_from_type(MBEDTLS_MD_SHA512);
 	mbedtls_md_context_t md_ctx;
 	std::string byte;
 	size_t j = 0;
+	int result = 0;
 
 	for (size_t i = 0; i < salt.length(); i += char_conversion, ++j) {
 		byte = salt.substr(i, char_conversion);
@@ -185,11 +191,11 @@ bool ServerManager::userAuthentication(std::string userName, std::string passwor
 	mbedtls_md_init(&md_ctx);
 	mbedtls_md_setup(&md_ctx, md_info, 1);
 	mbedtls_pkcs5_pbkdf2_hmac(&md_ctx, reinterpret_cast<const unsigned char*>(password.c_str()), password.length(), 
-		salt_char, 32, NUMBER_OF_ITERATIONS, 64, pbkdf2_output);
+		salt_char, SALT_LENGTH, NUMBER_OF_ITERATIONS, PBKDF2_LENGTH, pbkdf2_output);
 
 	mbedtls_md_free(&md_ctx);
 
-	if (memcpy(pbkdf2_output, row_hash_char, 32) == 0)
+	if (memcpy(pbkdf2_output, row_hash_char, PBKDF2_LENGTH) == 0)
 	{
 		return true;
 	}
@@ -210,7 +216,15 @@ Client* ServerManager::clientConnect(unsigned socket) {
 void ServerManager::clientDisconnect(Client* client) {
 	//TODO Marek
 	//Vymaz z kontajneru, delete object
+	if (client == nullptr)
+	{
+		std::cerr << "Client is null\n";
+		return;
+	}
 
+	std::string message = "You will be disconected\n";
+
+	client->sendMessage(message.length(), reinterpret_cast<const unsigned char*>(message.c_str()));
 	auto it = std::find(m_clients.begin(), m_clients.end(), client);
 	m_clients.erase(it);
 	delete client;
@@ -219,19 +233,35 @@ void ServerManager::clientDisconnect(Client* client) {
 bool ServerManager::clientLogIn(Client* client, std::string userName, std::string password) {
 	//TODO Marek
 	//Zavolaj userAuthentication a pripadne logInUser na Client
-
+	if (client == nullptr)
+	{
+		std::cerr << "Client is null\n";
+		return;
+	}
+	
+	std::string message; 
+	
 	if (userAuthentication(userName, password))
 	{
+		message = "Login successful\n";
+		client->sendMessage(message.length(), reinterpret_cast<const unsigned char*>(message.c_str()));
 		client->logInUser(userName);
 		return true;
 	}
-
+	message = "Invalid login data\n";
+    client->sendMessage(message.length(), reinterpret_cast<const unsigned char*>(message.c_str()));
 	return false;
 }
 
 void ServerManager::clientLogOut(Client* client) {
 	//TODO Marek
 	//logOutUser na Client
-
+	if(client == nullptr)
+	{
+		std::cerr << "Client is null\n";
+		return;
+	}
+	std::string message = "You were logged off\n";
+	client->sendMessage(message.length(), reinterpret_cast<const unsigned char*>(message.c_str()));
 	client->logOutUser();
 }
