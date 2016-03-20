a7a70776491aa40e2730716fa259af4ca71284df
Khabos <kubo.martinka@gmail.com>
2016-03-20 20:51:58 +0100

    database test2

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -7,12 +7,12 @@
 
 TEST_CASE("Database tests") {
 	SECTION("Constructor database") {
-		CHECK_THROWS_AS(Database nokDb("./a/b/c/d/db_doesnt_exist"), DatabaseAccessForbidden);
-		CHECK_NOTHROW(Database okDb("dbtest123456789ok"));
+		CHECK_THROWS_AS(Database nokDb("./a/b/c/d/db_doesnt_exist.db"), DatabaseAccessForbidden);
+		CHECK_NOTHROW(Database okDb("dbtest123456789ok.db"));
 	}
 
 	SECTION("Database write/read") {
-		Database myDB("myDB_test");
+		Database myDB("myDB_rw.db");
 		myDB.insertUser(UserDatabaseRow("testname", "testpassword", "testsalt"));
 		myDB.insertUser(UserDatabaseRow("testname2", "testpassword2", "testsalt2"));
 		UserDatabaseRow row = myDB.getUser("testname2");
@@ -23,11 +23,11 @@ TEST_CASE("Database tests") {
 	}
 
 	SECTION("Load database") {
-		Database myDB("myDB_test");
+		Database myDB("myDB_load.db");
 		myDB.insertUser(UserDatabaseRow("testname", "testpassword", "testsalt"));
 		myDB.insertUser(UserDatabaseRow("testname2", "testpassword2", "testsalt2"));
 		myDB.~Database();
-		Database myDB_open("myDB_test");
+		Database myDB_open("myDB_load.db");
 		UserDatabaseRow row_open = myDB_open.getUser("testname");
 		CHECK(row_open.exists() == true);
 		CHECK(row_open.getName().compare("testname") == 0);
@@ -36,15 +36,48 @@ TEST_CASE("Database tests") {
 	}
 
 	SECTION("clear database") {
-		Database myDB("myDB_to_clear");
+		Database myDB("myDB_clear.db");
 		myDB.insertUser(UserDatabaseRow("testname", "testpassword", "testsalt"));
 		myDB.insertUser(UserDatabaseRow("testname2", "testpassword2", "testsalt2"));
-		myDB.clearDatabase();
 		UserDatabaseRow row = myDB.getUser("testname");
+		CHECK(row.exists() == true);
+		CHECK(row.getName().compare("testname") == 0);
+		CHECK(row.getPassword().compare("testpassword") == 0);
+		CHECK(row.getName().compare("testsalt") == 0);
+		row = myDB.getUser("testname2");
+		CHECK(row.exists() == true);
+		CHECK(row.getName().compare("testname2") == 0);
+		CHECK(row.getPassword().compare("testpassword2") == 0);
+		CHECK(row.getName().compare("testsalt2") == 0);
+		myDB.clearDatabase();
+		row = myDB.getUser("testname");
 		CHECK(row.exists() == false);
 		CHECK(row.getName().compare("testname") != 0);
 		CHECK(row.getPassword().compare("testpassword") != 0);
 		CHECK(row.getName().compare("testsalt") != 0);
+		row = myDB.getUser("testname2");
+		CHECK(row.exists() == false);
+		CHECK(row.getName().compare("testname2") != 0);
+		CHECK(row.getPassword().compare("testpassword2") != 0);
+		CHECK(row.getName().compare("testsalt2") != 0);
+
+	}
+
+	SECTION("database delete user") {
+		Database myDB("myDB_deleteUser.db");
+		myDB.insertUser(UserDatabaseRow("testname", "testpassword", "testsalt"));
+		myDB.insertUser(UserDatabaseRow("testname2", "testpassword2", "testsalt2"));
+		UserDatabaseRow row = myDB.getUser("testname2");
+		CHECK(row.exists() == true);
+		CHECK(row.getName().compare("testname2") == 0);
+		CHECK(row.getPassword().compare("testpassword2") == 0);
+		CHECK(row.getName().compare("testsalt2") == 0);
+		myDB.removeUser("testname2");
+		row = myDB.getUser("testname2");
+		CHECK(row.exists() == false);
+		CHECK(row.getName().compare("testname2") != 0);
+		CHECK(row.getPassword().compare("testpassword2") != 0);
+		CHECK(row.getName().compare("testsalt2") != 0);
 	}
 }
 
@@ -54,12 +87,12 @@ TEST_CASE("Server tests") {
 		std::string pw[5] = { "password0", "password1", "password2", "password3", "password4" };
 
 	SECTION("Constructor database") {
-		CHECK_THROWS_AS(ServerManager nokserver("./a/b/c/d/db_doesnt_exist2"), DatabaseAccessForbidden);
-		CHECK_NOTHROW(ServerManager nokserver("dbtest123456789ok2"));
+		CHECK_THROWS_AS(ServerManager nokserver("./a/b/c/d/db_doesnt_exist2.db"), DatabaseAccessForbidden);
+		CHECK_NOTHROW(ServerManager nokserver("dbtest123456789ok2.db"));
 		}
 
 	SECTION("User registration") {
-		ServerManager myServer("test_database");
+		ServerManager myServer("test_database1.db");
 		CHECK(myServer.userRegistration("John", "a") == false);
 		CHECK(myServer.userRegistration("Jack", "length7") == false);
 
@@ -80,7 +113,7 @@ TEST_CASE("Server tests") {
 	}
 
 	SECTION("User authentication") {
-		ServerManager myServer("test_database");
+		ServerManager myServer("test_database2.db");
 		for (int i = 0; i < 5; ++i) {
 			myServer.userRegistration(names[i], pw[i]);
 		}
@@ -93,7 +126,7 @@ TEST_CASE("Server tests") {
 	}
 
 	SECTION("List of online users") {
-		ServerManager myServer("test_database");
+		ServerManager myServer("test_database3.db");
 		for (int i = 0; i < 5; ++i) {
 			myServer.userRegistration(names[i], pw[i]);
 		}
